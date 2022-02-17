d538ac20fbadca4e570bbea2da3f19e8dc22fac8
Annamalai <imemyselfanna@gmail.com>
2022-02-17 22:09:09 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,69 @@
+let numerator = 0,denominator = 0;
+
+function assignValue(val,position){
+    if(position=="numerator"){
+        numerator=numerator*10+val;
+        document.getElementById(position).value = numerator;
+    }
+    else{
+        denominator=denominator*10+val;
+        document.getElementById(position).value = denominator;
+    }
+}
+
+function clearLast(position){
+    if(position=="numerator"){
+        numerator=Math.floor(numerator/10);
+        if(numerator==0){
+            document.getElementById(position).value = '';
+        }
+        else{
+            document.getElementById(position).value = numerator;
+        }
+    }
+    else{
+        denominator=Math.floor(denominator/10);
+        if(denominator!=0){
+            document.getElementById(position).value = denominator;
+        }
+        else{
+            document.getElementById(position).value = '';    
+        }
+    }
+}
+
+function allClear(position){
+    if(position=="numerator"){
+        numerator = 0;
+        document.getElementById(position).value = '';
+    }
+    else{
+        denominator = 0;
+        document.getElementById(position).value = '';
+    }
+}
+
+function getQuotientRemainder(){
+    if(numerator!=0 && denominator!=0){
+        quotient = Math.floor(numerator/denominator);
+        remainder = numerator%denominator;
+        document.getElementById("resultValue").innerHTML="<div class=\"result-box\"><p>Quotient = "+ quotient +"<br><br> Remainder = "+ remainder +"</p><br><button onclick=\"reset()\" class=\"calculate-btn\">Reset</button></div>";
+    }
+    else if(numerator==0 && denominator==0){
+        document.getElementById("resultValue").innerHTML="<div class=\"result-box\"><p>Enter value > 0 for numerator and denominator</p><br><button onclick=\"reset()\" class=\"calculate-btn\">Reset</button></div>";
+    }
+    else if(numerator==0){
+        document.getElementById("resultValue").innerHTML="<div class=\"result-box\"><p>Enter value > 0 for numerator</p><br><button onclick=\"reset()\" class=\"calculate-btn\">Reset</button></div>";
+    }
+    else{
+        document.getElementById("resultValue").innerHTML="<div class=\"result-box\"><p>Enter value > 0 for denominator</p><br><button onclick=\"reset()\" class=\"calculate-btn\">Reset</button></div>";
+    }
+}
+
+function reset(){
+    location.replace("index.html");
+}
+
+function abc(){
+    alert(1);
+}
